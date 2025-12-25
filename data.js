9712e0e9963629ce0938d904178491265abe0cfa
jhshin-code <jklshin1234@gmail.com>
2025-12-25 22:24:35 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const scheduleData = [
+  { week: "12월 1주", piano: "김철수", praise: "이영희", dance: "박지민", hand: "최유진" },
+  { week: "12월 2주", piano: "이영희", praise: "박지민", dance: "최유진", hand: "김철수" },
+  { week: "12월 3주", piano: "박지민", praise: "최유진", dance: "김철수", hand: "이영희" },
+  { week: "12월 4주", piano: "최유진", praise: "김철수", dance: "이영희", hand: "박지민" },
+  { week: "12월 5주", piano: "김철수", praise: "이영희", dance: "박지민", hand: "최유진" },
+];
